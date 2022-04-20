d01ffca5d8d1ced88f8b4fdcb3ce831940fa4f65
frontalice <mayonakaTV@me.com>
2022-04-20 11:57:48 +0900

    Initial Commit

---
+++ MOSAIC LIFE Rev2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOSAIC LIFE Rev2
+//
+//  Created by Toshiki Hanakawa on 2022/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
